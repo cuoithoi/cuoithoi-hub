7a1a00255554202f5ccfbe0416e9c2f7cd0f16a1
Quanabcd <37532986+Quanabcd@users.noreply.github.com>
2023-06-21 10:24:58 +0700

    update

--- src/components/loginSocial.js
+++ src/components/loginSocial.js
@@ -1,12 +1,31 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import Languages from '@/commons/Languages'
 import { Button } from '@/components/button'
 import IcFacebook from '@/assets/home-image/IcFacebook.svg'
 import IcGoogle from '@/assets/home-image/IcGoogle.svg'
 import FacebookLogin from '@greatsumini/react-facebook-login'
-import { GoogleLogin, GoogleOAuthProvider } from '@react-oauth/google';
+import GoogleLogin from 'react-google-login';
+import { gapi } from 'gapi-script'
 
 const LoginSocial = () => {
+
+  useEffect(() => {
+
+    function start() {
+      gapi.client.init({
+        clientId: "714927639601-9opts6sksavno6i1h6pjik4avt5vvnnj.apps.googleusercontent.com",
+        scope: ""
+      })
+    }
+
+    gapi.load('client:auth2', start)
+
+  }, [])
+
+  const responseGoogle = (response) => {
+    console.log(response);
+  }
+
   return (
     <div className='otherLoginSocial'>
       <div className='titleOrther'>
@@ -29,7 +48,7 @@ const LoginSocial = () => {
       <FacebookLogin
         appId="6234250993358177"
         onSuccess={(response) => {
-          console.log('Login Success!', response);
+          console.log('Login Success!', response.profileObj);
         }}
         onFail={(error) => {
           console.log('Login Failed!', error);
@@ -51,21 +70,13 @@ const LoginSocial = () => {
           }
         />
       </FacebookLogin>
-      <GoogleOAuthProvider clientId="714927639601-fmahu1dtpmssop1omn4f99shjk1jnm3q.apps.googleusercontent.com">
-
-        <GoogleLogin
-          onSuccess={credentialResponse => {
-            console.log(credentialResponse);
-          }}
-
-          onError={() => {
-            console.log('Login Failed');
-          }}
-          text={Languages.inputText.continueWithGG}
-          width='100%'
-        />
-
-      </GoogleOAuthProvider>
+      <GoogleLogin
+        clientId="714927639601-9opts6sksavno6i1h6pjik4avt5vvnnj.apps.googleusercontent.com"
+        buttonText="Login"
+        onSuccess={responseGoogle}
+        onFailure={responseGoogle}
+        cookiePolicy={'single_host_origin'}
+      />
 
     </div>
   )
